d3d03da329512f915e0a2d0b8a3aec0b169568ab
Vipul Agnihotri <86738105+vibgyorvip@users.noreply.github.com>
2022-10-03 20:14:11 +0530

    Create Alien.java

---
+++ ScopePrototype/Alien.java
@@ -0,0 +1,12 @@
+package com.programming.SpringDemo;
+
+public class Alien {
+	int age;
+	public Alien() {
+		System.out.println("Alien object created !!");
+	}
+	public void code()
+	{
+		System.out.println("coding ....");
+	}
+}

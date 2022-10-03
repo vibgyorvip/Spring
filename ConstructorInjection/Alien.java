f7abd19c94d07fc530cc34a18e102663b366eca0
Vipul Agnihotri <86738105+vibgyorvip@users.noreply.github.com>
2022-10-04 00:36:01 +0530

    Create Alien.java

---
+++ ConstructorInjection/Alien.java
@@ -0,0 +1,31 @@
+package com.programming.SpringDemo;
+
+public class Alien {
+	private int age;
+	
+	private Laptop laptop;
+	
+	public Alien(int age) {
+		this.age = age;
+		System.out.println("Parameterized constructor called");
+	}
+	public Laptop getLaptop() {
+		return laptop;
+	}
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+		System.out.println("age assigned !!");
+	}
+	
+	public void code()
+	{
+		System.out.println("coding ....");
+		laptop.compile();
+	}
+}
